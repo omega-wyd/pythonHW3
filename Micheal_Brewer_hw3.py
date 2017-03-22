9b575a1e68de091cac52d48596da0da75458a73d
Micheal Brewer <mbrewerramirez@mail.weber.edu>
2017-03-22 17:44:22 -0600

    started the assignment

---
+++ Micheal_Brewer_hw3.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 student1 <student1@CS3030_87>
+#
+
+
+#===========================================================================
+#
+#       File: Micheal_Brewer_hw3.py 
+#
+#       Usage: ./Micheal_Brewer_hw3.py
+#
+#   Description:
+#
+#       Options: ---
+#       ReqOPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Micheal Brewer (), mbrewerramirez@mail.weber.edu
+#  ORGANIZATION:
+#       CREATED: 03/03/2017 14:27
+#      REVISION:  ---
+#===========================================================================
+
+import sys
+from urllib.request import urlopen
+
+
+
+
+
+
+
+
+
+
+#Main Function
+def main():
+    """
+    Test Function
+    """
+
+
+
+if __name__=="__main__":
+    # Call Main
+    main()
+
+    exit(0)
